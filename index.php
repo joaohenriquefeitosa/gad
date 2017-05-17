aba01a961f6c110fd270ca4a20ff733cab5fc89f
João Henrique Feitosa <joaohenriquefsf@gmail.com>
2017-05-16 21:15:06 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<p><img src="img/students.png" alt=""></p>
+		<h1>G.A.D. Gerenciador de Atividades Dinâmico</h1>
+	</header><!-- /header -->
+
+	<section>
+		<form>
+			<div id="email">
+			<label for=""><span>d</span></label>
+			<input type="" name="">
+			</div>
+			<div id="pass">
+			<label for=""><span>d</span></label>
+			<input type="" name="">
+			</div>
+			<div id="enviar"><p>Login</p> <img src="img/login.png" alt=""><div id="clear"></div></div>
+
+		</form>
+	</section>
+	
+</body>
+</html>
