05aeca0de230abd8abff32d05ba9f955f794b54e
João Henrique Feitosa <joaohenriquefsf@gmail.com>
2017-06-11 00:42:24 -0300

    Classe de cadastro de novos usuarios concluida

--- _app/Models/Cadastro.class.php
+++ _app/Models/Cadastro.class.php
@@ -10,6 +10,8 @@
 
 class Cadastro{
 
+	private $Nome;
+	private $Curso;
 	private $Email;
 	private $Senha;
 	private $Error;
@@ -19,8 +21,18 @@ class Cadastro{
 
 	}
 
-	public function ExeLogin(){
+	public function ExeCadastro(array $UserData){
+		$this->Nome = (string) $UserData['user'];
+		$this->Email = (string) $UserData['email'];
+		$this->Curso = (string) $UserData['curso'];
+		$this->Senha = (string) $UserData['pass'];
 
+		$this->Cadastrar();
+
+	}
+
+	public function getError(){
+		return $this->Error;
 	}
 
 	public function getResult(){
@@ -32,20 +44,57 @@ class Cadastro{
 	 * ************ PRIVATE METHODS ***********
 	 * ****************************************
 	 */
-	
-	public function setLogin(){
+	private function Cadastrar(){
+		$this->Senha = md5($this->Senha);
+
+		/*
+		if(!getUser()):
+			$this->Error = ['Este email já esta sendo utilizado!', GD_ALERT];
+		else:
+			$this->Execute();
+		endif;
+		*/
+		
+		$create = new Create();
+
+		$query= ['c_nomeprof' => $this->Nome, 'c_mailprof' => $this->Email, 'b_admiprof' => 1 , 'c_passprof' => $this->Senha];
+
+
+		$create -> ExeCreate('professor', $query);
+
+		if($create->getResult()):
+			$this->Result = $create->getResult()[0];
+			return true;
+		else:
+			return false;
+		endif;
 
 	}
+	/*
+	private function getUser(){
+		
+		$read = new Read();
+		$read->ExeRead('professor', "WHERE c_mailprof = :e", "e={$this->Email}");
 
-	public function getUser(){
+		if($read->getResult()):
+			return true;
+		else:
+			return false;
+		endif;
 
 	}
+	*/
 
 	private function Execute(){
 		if(!session_id()):
 			session_start();
 		endif;
 
+		$this->Cadastrar();
+
+		$_SESSION['userCadastro'] = $this->Result;
+		$this->Error = ["Olá {$this->Result['c_nomeprof']}, seja bem vindo(a)."];
+		$this->Result = true;
 
 	}
 }
--- cadastro.php
+++ cadastro.php
@@ -1,3 +1,7 @@
+<?php 
+	session_start();
+ ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,38 +18,69 @@
 	</header><!-- /header -->
 
 	<?php 
+		require('./_app/Config.inc.php');
 
-	echo md5(123456);
+		$cadastro = new Cadastro();
+
+		$dataCadastro = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+		if(!empty($dataCadastro['submitCadastro'])):
+				$cadastro->ExeCadastro($dataCadastro);
+			if(!$cadastro->getResult()):
+				GErro($cadastro->getError()[0], $cadastro->getError()[1]);
+			else:
+				header('Location: painel_aluno.php');
+			endif;				
+		endif;
+		/*
+
+		
+
+			if(!$cadastro->getResult()):
+				GErro($cadastro->getError()[0], $cadastro->getError()[1]);
+			else:
+				header('Location: painel_admin.php');
+			endif;
+
+		
+		*/
+
+
+		echo "<pre>";
+		print_r($cadastro);
+		print_r($dataCadastro);
+		echo "</pre>";
 	 ?>
 
 
+
 	<section>
-		<form id="logcad">
+		<form id="logcad" method="post">
 			<div id="nome">
 			<label for="iuser"><span><img src="img/user.png" alt=""></span></label>
-			<input type="text" name="nuser" id="iuser" value="seu nome...">
+			<input type="text" name="user" id="iuser" value="seu nome...">
 			</div>
 
 			<div id="email">
 			<label for="iemail"><span><img src="img/email.png" alt=""></span></label>
-			<input type="text" name="nuser" id="iemail" value="email...">
+			<input type="text" name="email" id="iemail" value="email...">
 			</div>
 
 			<div id="curso">
 			<label for="icurso"><span><img src="img/course.png" alt=""></span></label>
-			<select name="ncurso" id="icurso">
+			<select name="curso" id="icurso">
 				<option value="" selected>SEU CURSO</option>
-				<option value="">TADS</option>
-				<option value="">TADS</option>
+				<option value="primeiro_tads">TADS</option>
+				<option value="segundo_tads">TADS</option>
 			</select>
 			</div>
 
 
 			<div id="pass">
 			<label for="ipass"><span><img src="img/padlock.png" alt="" width="20px" height="20px"></span></label>
-			<input type="text" name="npass" id="ipass" value="senha...">
+			<input type="text" name="pass" id="ipass" value="senha...">
 			</div>
-
+			<input type="submit" name="submitCadastro" value="Enviar">
 			<div id="enviar"><p>Cadastre-se</p> <img src="img/login.png" alt=""><div id="clear"></div></div>
 			<div id="troca">Não é cadastrado? <a href="cadastro.php"><small>Clique aqui!</small></a></div>
 		</form>
